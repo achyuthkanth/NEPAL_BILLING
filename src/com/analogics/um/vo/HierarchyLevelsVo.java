19507d5577583a00203b817321f55c5dd89b1989
achyuthkanth <softwaresupport@analogicgroup.com>
2020-12-03 17:21:39 +0530

    Hierarchies

---
+++ src/com/analogics/um/vo/HierarchyLevelsVo.java
@@ -0,0 +1,290 @@
+package com.analogics.um.vo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HierarchyLevelsVo {
+	private String level1Id="-1";
+	private String level2Id="-1";
+	private String level3Id="-1";
+	private String level4Id="-1";
+	private String level5Id="-1";
+	private String level6Id="-1";
+	private String level7Id="-1";
+	private String level8Id="-1";
+	private String level9Id="-1";
+	private String level10Id="-1";
+	private String level11Id="-1";
+	private String level12Id="-1";
+	private String level13Id="-1";
+	private String level14Id="-1";
+	private String level15Id="-1";
+	
+	private Map<String,String> level1Map=new HashMap<String,String>();
+	private Map<String,String> level2Map=new HashMap<String,String>();
+	private Map<String,String> level3Map=new HashMap<String,String>();
+	private Map<String,String> level4Map=new HashMap<String,String>();
+    private Map<String,String> level5Map=new HashMap<String,String>();
+	private Map<String,String> level6Map=new HashMap<String,String>();
+	private Map<String,String> level7Map=new HashMap<String,String>();
+	private Map<String,String> level8Map=new HashMap<String,String>();
+	private Map<String,String> level9Map=new HashMap<String,String>();
+	private Map<String,String> level10Map=new HashMap<String,String>();
+	private Map<String,String> level11Map=new HashMap<String,String>();
+	private Map<String,String> level12Map=new HashMap<String,String>();
+	private Map<String,String> level13Map=new HashMap<String,String>();
+	private Map<String,String> level14Map=new HashMap<String,String>();
+	private Map<String,String> level15Map=new HashMap<String,String>();
+	
+	
+	private String hLevelId;
+	private String hLevelName;
+	private String hLevelCode;
+	private String hLevelUniqueCode;
+	private String hPreviousLevelId;
+	private LevelIndexMaster levelIndexObj;
+	private String levelCount;
+	private String levelCode;
+	private String levelName;
+	private String levelUniqueCode;
+	public String getLevel1Id() {
+		return level1Id;
+	}
+	public void setLevel1Id(String level1Id) {
+		this.level1Id = level1Id;
+	}
+	public String getLevel2Id() {
+		return level2Id;
+	}
+	public void setLevel2Id(String level2Id) {
+		this.level2Id = level2Id;
+	}
+	public String getLevel3Id() {
+		return level3Id;
+	}
+	public void setLevel3Id(String level3Id) {
+		this.level3Id = level3Id;
+	}
+	public String getLevel4Id() {
+		return level4Id;
+	}
+	public void setLevel4Id(String level4Id) {
+		this.level4Id = level4Id;
+	}
+	public String getLevel5Id() {
+		return level5Id;
+	}
+	public void setLevel5Id(String level5Id) {
+		this.level5Id = level5Id;
+	}
+	public String getLevel6Id() {
+		return level6Id;
+	}
+	public void setLevel6Id(String level6Id) {
+		this.level6Id = level6Id;
+	}
+	public String getLevel7Id() {
+		return level7Id;
+	}
+	public void setLevel7Id(String level7Id) {
+		this.level7Id = level7Id;
+	}
+	public String getLevel8Id() {
+		return level8Id;
+	}
+	public void setLevel8Id(String level8Id) {
+		this.level8Id = level8Id;
+	}
+	public String getLevel9Id() {
+		return level9Id;
+	}
+	public void setLevel9Id(String level9Id) {
+		this.level9Id = level9Id;
+	}
+	public String getLevel10Id() {
+		return level10Id;
+	}
+	public void setLevel10Id(String level10Id) {
+		this.level10Id = level10Id;
+	}
+	public String getLevel11Id() {
+		return level11Id;
+	}
+	public void setLevel11Id(String level11Id) {
+		this.level11Id = level11Id;
+	}
+	public String getLevel12Id() {
+		return level12Id;
+	}
+	public void setLevel12Id(String level12Id) {
+		this.level12Id = level12Id;
+	}
+	public String getLevel13Id() {
+		return level13Id;
+	}
+	public void setLevel13Id(String level13Id) {
+		this.level13Id = level13Id;
+	}
+	public String getLevel14Id() {
+		return level14Id;
+	}
+	public void setLevel14Id(String level14Id) {
+		this.level14Id = level14Id;
+	}
+	public String getLevel15Id() {
+		return level15Id;
+	}
+	public void setLevel15Id(String level15Id) {
+		this.level15Id = level15Id;
+	}
+	public Map<String, String> getLevel1Map() {
+		return level1Map;
+	}
+	public void setLevel1Map(Map<String, String> level1Map) {
+		this.level1Map = level1Map;
+	}
+	public Map<String, String> getLevel2Map() {
+		return level2Map;
+	}
+	public void setLevel2Map(Map<String, String> level2Map) {
+		this.level2Map = level2Map;
+	}
+	public Map<String, String> getLevel3Map() {
+		return level3Map;
+	}
+	public void setLevel3Map(Map<String, String> level3Map) {
+		this.level3Map = level3Map;
+	}
+	public Map<String, String> getLevel4Map() {
+		return level4Map;
+	}
+	public void setLevel4Map(Map<String, String> level4Map) {
+		this.level4Map = level4Map;
+	}
+	public Map<String, String> getLevel5Map() {
+		return level5Map;
+	}
+	public void setLevel5Map(Map<String, String> level5Map) {
+		this.level5Map = level5Map;
+	}
+	public Map<String, String> getLevel6Map() {
+		return level6Map;
+	}
+	public void setLevel6Map(Map<String, String> level6Map) {
+		this.level6Map = level6Map;
+	}
+	public Map<String, String> getLevel7Map() {
+		return level7Map;
+	}
+	public void setLevel7Map(Map<String, String> level7Map) {
+		this.level7Map = level7Map;
+	}
+	public Map<String, String> getLevel8Map() {
+		return level8Map;
+	}
+	public void setLevel8Map(Map<String, String> level8Map) {
+		this.level8Map = level8Map;
+	}
+	public Map<String, String> getLevel9Map() {
+		return level9Map;
+	}
+	public void setLevel9Map(Map<String, String> level9Map) {
+		this.level9Map = level9Map;
+	}
+	public Map<String, String> getLevel10Map() {
+		return level10Map;
+	}
+	public void setLevel10Map(Map<String, String> level10Map) {
+		this.level10Map = level10Map;
+	}
+	public Map<String, String> getLevel11Map() {
+		return level11Map;
+	}
+	public void setLevel11Map(Map<String, String> level11Map) {
+		this.level11Map = level11Map;
+	}
+	public Map<String, String> getLevel12Map() {
+		return level12Map;
+	}
+	public void setLevel12Map(Map<String, String> level12Map) {
+		this.level12Map = level12Map;
+	}
+	public Map<String, String> getLevel13Map() {
+		return level13Map;
+	}
+	public void setLevel13Map(Map<String, String> level13Map) {
+		this.level13Map = level13Map;
+	}
+	public Map<String, String> getLevel14Map() {
+		return level14Map;
+	}
+	public void setLevel14Map(Map<String, String> level14Map) {
+		this.level14Map = level14Map;
+	}
+	public Map<String, String> getLevel15Map() {
+		return level15Map;
+	}
+	public void setLevel15Map(Map<String, String> level15Map) {
+		this.level15Map = level15Map;
+	}
+	public String gethLevelId() {
+		return hLevelId;
+	}
+	public void sethLevelId(String hLevelId) {
+		this.hLevelId = hLevelId;
+	}
+	public String gethLevelName() {
+		return hLevelName;
+	}
+	public void sethLevelName(String hLevelName) {
+		this.hLevelName = hLevelName;
+	}
+	public String gethLevelCode() {
+		return hLevelCode;
+	}
+	public void sethLevelCode(String hLevelCode) {
+		this.hLevelCode = hLevelCode;
+	}
+	public String gethLevelUniqueCode() {
+		return hLevelUniqueCode;
+	}
+	public void sethLevelUniqueCode(String hLevelUniqueCode) {
+		this.hLevelUniqueCode = hLevelUniqueCode;
+	}
+	public String gethPreviousLevelId() {
+		return hPreviousLevelId;
+	}
+	public void sethPreviousLevelId(String hPreviousLevelId) {
+		this.hPreviousLevelId = hPreviousLevelId;
+	}
+	public LevelIndexMaster getLevelIndexObj() {
+		return levelIndexObj;
+	}
+	public void setLevelIndexObj(LevelIndexMaster levelIndexObj) {
+		this.levelIndexObj = levelIndexObj;
+	}
+	public String getLevelCode() {
+		return levelCode;
+	}
+	public void setLevelCode(String levelCode) {
+		this.levelCode = levelCode;
+	}
+	public String getLevelName() {
+		return levelName;
+	}
+	public void setLevelName(String levelName) {
+		this.levelName = levelName;
+	}
+	public String getLevelUniqueCode() {
+		return levelUniqueCode;
+	}
+	public void setLevelUniqueCode(String levelUniqueCode) {
+		this.levelUniqueCode = levelUniqueCode;
+	}
+	public String getLevelCount() {
+		return levelCount;
+	}
+	public void setLevelCount(String levelCount) {
+		this.levelCount = levelCount;
+	}
+}
